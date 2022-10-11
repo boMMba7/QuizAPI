95575845c5aecd43445fb62c412a03234d4805f4
Fabio Pontes <65029985+boMMba7@users.noreply.github.com>
2022-10-11 16:28:27 +0100

    End Point post
    this end point '/api/questions' allow to add new question to question collection, verifying if question is valid:
    if contain questions at least 2 answer, and no empty strings.. etc

--- Quiz.js
+++ Quiz.js
@@ -33,8 +33,44 @@ quizApp.get('/api/questions/:category', (req, res) => {
 	// check if category exist, if not return imediatly
 	const result = questions.filter(question => question.category === req.params.category);
 	if(result.length === 0) return res.status(404).send('no question with this category');
+	
 	res.send(result);
 });
+
+//adding new question
+quizApp.post('/api/questions', (req, res) => {
+	// { error } object distroction, allow to pick just the propiete of returned object
+	const { error } = validateQuestion(req.body);
+	if(error) return res.status(400).send(error.details[0].message);
+
+	const question = {
+		id: questions.length + 1,
+		category: req.body.category,
+		question: req.body.question,
+		answer: req.body.answer
+	};
+	questions.push(question);
+	res.send(question);
+});
+
+function validateQuestion(question) {
+	
+	//items are string, minimocharacter 1 answer is mandatory
+	const schemaAnswer = Joi.object({
+		answer: Joi.string().min(1).required()
+	});
+
+	//creating rulls to validate the question
+	const schemaQ = Joi.object({
+		//category is String with minimo 3 characters ans is mandatory
+		category: Joi.string().min(3).required(),
+		question: Joi.string().min(3).required(),
+		// answer is array with min 2 items and max 4 item
+		answer: Joi.array().min(2).max(4).items( schemaAnswer ).required()
+	});
+	//validating the question, if pass all Joi condition return true
+	return schemaQ.validate(question);
+}
 
 
 //reading the enveriment port, if dont have atribut port 3000
